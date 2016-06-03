8de7c55da16b9043ca053efcf32d9532a4da1016
Mariano Heller <marianoheller91@gmail.com>
2016-06-03 16:32:14 -0300

    Added borrar popup.

--- views/cursos/index.php
+++ views/cursos/index.php
@@ -1,25 +1,39 @@
-<p>Listado de cursos</p>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Listado de cursos</title>
+    <script type="text/javascript">
+        function confirmar() {
+            return confirm("¿Desea borrar el registro?");
+        }
+    </script>
+</head>
+<body>
+    <p>Listado de cursos</p>
 
-<table border="1">
-    <thead>
+    <table border="1">
+        <thead>
         <th>Nombre</th>
         <th>Turno</th>
         <th>Cantidad alumnos</th>
         <th>Accion</th>
-    </thead>
-    <tbody>
-<?php /** @var Curso $curso */ foreach($cursos as $curso) { ?>
-        <tr>
-            <td><?php echo $curso->nombre;?></td>
-            <td><?php echo $curso->turno;?></td>
-            <td><?php echo "aa";?></td>
-            <td>
-                <a href='?controller=cursos&action=show&id=<?php echo $curso->id; ?>'>Ver</a>|
-                <a href='?controller=cursos&action=edit&id=<?php echo $curso->id; ?>'>Editar</a>|
-                <a href='?controller=cursos&action=borrar&id=<?php echo $curso->id; ?>'>Borrar</a>|
-            </td>
-        </tr>
-<?php } ?>
-    </tbody>
-</table>
+        </thead>
+        <tbody>
+        <?php /** @var Curso $curso */ foreach($cursos as $curso) { ?>
+            <tr>
+                <td><?php echo $curso->nombre;?></td>
+                <td><?php echo $curso->turno;?></td>
+                <td><?php echo "aa";?></td>
+                <td>
+                    <a href='?controller=cursos&action=show&id=<?php echo $curso->id; ?>'>Ver</a>|
+                    <a href='?controller=cursos&action=edit&id=<?php echo $curso->id; ?>'>Editar</a>|
+                    <a href='?controller=cursos&action=borrar&id=<?php echo $curso->id; ?>' onClick="return confirmar();">Borrar</a>
+                </td>
+            </tr>
+        <?php } ?>
+        </tbody>
+    </table>
+</body>
+</html>
 
