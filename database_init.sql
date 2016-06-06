70c69e606b274b6d538235a3c17ab65d4ca1272d
Mariano Heller <marianoheller91@gmail.com>
2016-06-06 17:40:52 -0300

    Added data to sql init script.

--- database_init.sql
+++ database_init.sql
@@ -20,9 +20,14 @@ INSERT INTO curso (nombre, turno) VALUES ('Professional Webmaster', 'Noche');
 INSERT INTO curso (nombre, turno) VALUES ('Experto Universitario en PHP', 'Tarde');
 INSERT INTO curso (nombre, turno) VALUES ('Professional Webmaster', 'Tarde');
 INSERT INTO curso (nombre, turno) VALUES ('Professional Webmaster (vacio)', 'Mañana');
+INSERT INTO curso (nombre, turno) VALUES ('Experto Universitario en PHP', 'Mañana');
+INSERT INTO curso (nombre, turno) VALUES ('Experto Universitario en PHP', 'Noche');
 
 INSERT INTO alumno(nombre, edad, curso_id) VALUES('Juan Perez', 25, 1);
 INSERT INTO alumno(nombre, edad, curso_id) VALUES('Jorge Gonzalez', 30, 2);
 INSERT INTO alumno(nombre, edad, curso_id) VALUES('Maria Lopez', 28, 3);
 INSERT INTO alumno(nombre, edad, curso_id) VALUES('Julian Sanchez', 34, 1);
 INSERT INTO alumno(nombre, edad, curso_id) VALUES('Carlos SinCurso', 38, NULL);
+INSERT INTO alumno(nombre, edad, curso_id) VALUES('Julian Heras', 24, 2);
+INSERT INTO alumno(nombre, edad, curso_id) VALUES('Roberto Sanchez', 34, 3);
+INSERT INTO alumno(nombre, edad, curso_id) VALUES('Juan Carlos', 35, 1);
